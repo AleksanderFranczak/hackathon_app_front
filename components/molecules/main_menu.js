c06e2d5ea091a1fa53edd7da0b307bbf150ed328
Aleksander Franczak <aleksanderfranczak@Aleksanders-MacBook-Air.local>
2022-11-04 19:50:27 +0100

    initial setup

---
+++ components/molecules/main_menu.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div``;
+
+const MainMenu = () => {
+  return <Container></Container>;
+};
+
+export default MainMenu;
